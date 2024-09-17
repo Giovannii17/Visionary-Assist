293d01075344863c2e5a857b5fb2be1a34a14448
Giovannii17 <giovannisilvasantos173@gmail.com>
2024-09-17 16:51:52 -0300

    Ultimos Detalhes
    Finalizado

--- Serviço/servico.js
+++ Serviço/servico.js
@@ -15,24 +15,17 @@ document.getElementById('support-link').addEventListener('click', function() {
     window.location.href = '../Suporte/suporte.html';
 });
 
-
-// Seleciona todos os elementos interativos (botões, links e inputs)
-const interactiveElements = document.querySelectorAll('a, button, input, textarea, select');
-
 // Adiciona a navegação por teclado e foco visível
 interactiveElements.forEach(element => {
     element.addEventListener('focus', () => {
-        // Adiciona uma classe para dar destaque ao foco
         element.classList.add('foco-visivel');
     });
 
     element.addEventListener('blur', () => {
-        // Remove a classe de destaque ao perder o foco
         element.classList.remove('foco-visivel');
     });
 
     element.addEventListener('keydown', (event) => {
-        // Verifica se a tecla Enter ou Espaço foi pressionada
         if (event.key === 'Enter' || event.key === ' ') {
             event.preventDefault();
             element.click();
